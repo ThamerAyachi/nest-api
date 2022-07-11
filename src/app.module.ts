35681868c0df0b342969ae989e4b5115a7f7d22a
Thamer Ayachi <thamerjak3@gmail.com>
2022-07-11 14:22:00 +0100

    Connecting to MySQL using TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomersModule } from './customors/customers.module';
+import entities from './typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [
+    CustomersModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'basxx1rmhng3swzpu2ol-mysql.services.clever-cloud.com',
+      port: 3306,
+      username: 'uir1the7ewe3ixrz',
+      password: 'tEygToo6zHvyjEwoIaCd',
+      database: 'basxx1rmhng3swzpu2ol',
+      entities: entities,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/typeorm/index.ts
@@ -0,0 +1,5 @@
+import { User } from './User';
+
+const entities = [User];
+
+export default entities;
